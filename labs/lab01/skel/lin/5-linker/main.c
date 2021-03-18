2644bf9c71a9cc36881d7dfdddd3c163758e7167
Bogdan <bogdan.p.deac@gmail.com>
2021-03-18 11:54:08 +0200

    add lab01 - bonus 3

--- labs/lab01/skel/lin/5-linker/main.c
+++ labs/lab01/skel/lin/5-linker/main.c
@@ -8,6 +8,19 @@
  */
 int str(void);
 
+/* The above declaration defines a symbol to a function 
+ * named str. In str.c is declared a char pointer to a 
+ * string, named std. A funciton call is a jump and 
+ * execute to an address indicated by a pointer. 
+ * A function name is a pointer to the first funciton's 
+ * instruction. At link time the linker verifies that there 
+ * is a definition for str() function, but at run-time, when 
+ * the function is called the execution jumps at the address 
+ * pointed by str pointer, which points to "SO" string 
+ * which is a memory location in .data segment -> segmentation 
+ * fault.
+ * */
+
 int main(void)
 {
 	str();
